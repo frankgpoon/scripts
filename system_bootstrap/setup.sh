31c583d1156123684eacf63d65ff1a9bbdbb80ef
Frank Poon <frankgpoon@users.noreply.github.com>
2023-03-10 22:16:09 -0500

    Redo bootstrap script (#2)
    * Separate setup into system and user setups
    
    * Fix errors

---
+++ system_bootstrap/setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+main() {
+  echo "Welcome to Frank's ssytem bootstrap script!"
+  echo "This script should be ran as root."
+
+  installDependencies
+  setupUserAccount
+}
+
+installDependencies() {
+  echo "Installing dependencies..."
+
+  # Setup git
+  apt-get install -yqq git
+  echo "🎉 Installed git"
+
+  echo "✅ Done installing"
+}
+
+setupUserAccount() {
+  echo "Enter the name of the user that you want to create:"
+  read -r username
+  useradd -m "$username"
+  echo "🎉 Created user $username"
+
+  while true; do
+    if passwd "$username"; then
+      echo "🎉 Added password for $username"
+      break
+    else
+      echo "❌ Password was not successfully changed"
+    fi
+  done
+
+  adduser "$username" sudo
+
+  echo "🎉 Added sudo privilege for $username"
+  
+  source_dir=$(dirname "$(dirname "$(realpath "$0")")")
+  dest_dir="/home/$username/"
+  cp -R "$source_dir" "$dest_dir"
+  chown -R "$username" "$dest_dir/$(basename "$source_dir")"
+
+
+  echo "✅ User was successfully created"
+
+  echo "This script is now finished. You should now login as $username"
+  echo "and continue setup via ~/$(basename "$source_dir")."
+}
+
+main
---
+++ user_bootstrap/setup.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+INSTALL_DIR="/home/$USER/.frankpoon"
+REPO_DIR="$INSTALL_DIR/repos"
+UPDATE_SCRIPTS_DIR="$INSTALL_DIR/update_scripts"
+RESOURCE_DIR="$INSTALL_DIR/resources"
+
+SSH_DIR="/home/$USER/.ssh"
+
+
+main() {
+
+  echo "Welcome to Frank's user bootstrap script!"
+  echo "---------------------------------------------"
+
+  setupEnv
+  setupClientSshKey
+  setupGithubSshKey
+  setupUpdater
+}
+
+setupClientSshKey() {
+  echo "If you want to add a client SSH key for this host, you can paste it here."
+  echo "Otherwise, a key will not be added."
+  read -r client_key
+
+  if [[ -n $client_key ]]; then
+    authorized_keys_path="$SSH_DIR/authorized_keys"
+
+    {
+      echo "# Client SSH key"
+      echo "$client_key"
+      echo
+    } >> "$authorized_keys_path"
+
+    echo "✅ Added client key to authorized keys"
+    echo -e "Add the following lines to ~/.ssh/config:\n"
+    echo "Host $(hostname -I | awk '{print $1}')"
+    echo "  Preferredauthentications publickey"
+    echo "  IdentityFile <path to your client ssh key>"
+    echo
+  else
+    echo "⚠️ Adding client key skipped"
+  fi
+}
+
+setupGithubSshKey() {
+  echo "Setting up Github SSH key..."
+
+  KEY_FILENAME="github"
+
+  private_key_path="$SSH_DIR/$KEY_FILENAME"
+  public_key_path="$SSH_DIR/$KEY_FILENAME.pub"
+  config_path="$SSH_DIR/config"
+
+  ssh-keygen -q -t ed25519 -C "$KEY_FILENAME" -P "" -f "$private_key_path"
+  {
+    echo "# Github SSH key"
+    echo "Host github.com"
+    echo "  Preferredauthentications publickey"
+    echo "  IdentityFile ~/.ssh/$KEY_FILENAME"
+    echo
+  } >> "$config_path"
+
+  echo "✅ Done. Please add the following SSH key into Github:"
+  echo -e "\n$(cat "$public_key_path")\n"
+}
+
+setupEnv() {
+  echo "Setting up environment for $USER..."
+
+  chsh -s /usr/bin/bash
+
+  mkdir "$INSTALL_DIR"
+  mkdir "$UPDATE_SCRIPTS_DIR"
+  mkdir "$REPO_DIR"
+  mkdir "$RESOURCE_DIR"
+
+  mkdir "$SSH_DIR"
+
+  echo "✅ Finished setting up environment for $USER."
+}
+
+setupUpdater() {
+  echo "Setting up auto-update directory and infrastructure for $USER..."
+  
+  source_dir="$(dirname "$0")/resources"
+  
+  cp "$source_dir/update_all.sh" "$INSTALL_DIR"
+  echo "✅ Created directories. You can put new update scripts under $UPDATE_SCRIPTS_DIR."
+
+  (crontab -l; echo "0 0 * * 0 $INSTALL_DIR/update_all.sh") | sort -u | crontab -
+}
+
+main
